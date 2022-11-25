6a86a05c9697f9930dd6abea121852c6898886b7
Wandonium <hillarywando@gmail.com>
2022-11-25 09:13:57 +0300

    Built a form input to take the url to shorten and generated a unique id for storing the url

--- app/public/index.php
+++ app/public/index.php
@@ -1,8 +1,40 @@
 <?php
-$pdo = new PDO('mysql:dbname=my_db;host=mysql', 'root', 'root@123', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
-$query = $pdo->query('SHOW VARIABLES like "version"');
+    function guidv4($data = null) {
+        // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
+        $data = $data ?? random_bytes(16);
+        assert(strlen($data) == 16);
 
-$row = $query->fetch();
+        // Set version to 0100
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+        // Set bits 6-7 to 10
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
 
-echo 'MySQL version:' . $row['Value'];
+        // Output the 36 character UUID.
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
+
+    if(!empty($_POST['url'])) {
+        echo $_POST['url'];
+        echo '<br/>' . uniqid();
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>My URL Shortener</title>
+</head>
+<body>
+    <form class="m-auto w-50 mt-5" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+        <div class="mb-3">
+            <input type="text" class="form-control" id="url-to-shorten" name="url" placeholder="Type a URL (content you want your shortened link to point to)" />
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    
+</body>
+</html>
