ae739d3ffda33b4dd98155682633e089874f5cd0
Wandonium <hillarywando@gmail.com>
2022-12-22 09:26:42 +0300

    Added mdb (https://mdbootstrap.com/docs/standard/forms/overview/) to register.php for the form

--- app/public/register.php
+++ app/public/register.php
@@ -13,6 +13,14 @@
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,700;1,400&display=swap" rel="stylesheet" /> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    rel="stylesheet"/>
+    <!-- MDB -->
+    <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.1/mdb.min.css"
+    rel="stylesheet"
+    />
     <link rel="stylesheet" href="register.css" />
     <style>
         html, body {
@@ -138,95 +146,76 @@
         </div>
     </nav>
     <main>
-        <div id="contentContainer" class="container">
-        <div class="row">
-            <div id="realContent" class="col-xs-12">
-            <div class="row">
-                <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-xs-12 col-sm-offset-0">
-                <h1>Sign Up</h1>
-                </div>
+    <form>
+        <!-- 2 column grid layout with text inputs for the first and last names -->
+        <div class="row mb-4">
+            <div class="col">
+            <div class="form-outline">
+                <input type="text" id="form3Example1" class="form-control" />
+                <label class="form-label" for="form3Example1">First name</label>
             </div>
-            <div class="row">
-                <section class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-xs-12 col-sm-offset-0">
-                <div class="well well-lg">
-                    <div class="row">
-                    <div class="col-sm-6 col-xs-12">
-                        <div class="row">
-                        <div class="col-xs-12">
-                            <h3>Welcome</h3></div>
-                        </div>
-                        <div class="row">
-                        <!-- Instructions -->
-                        <div class="col-xs-12">
-                            <p class="lead">Register now for <span class="text-success">FREE</span></p>
-                            <ul class="list-unstyled" style="line-height: 3; font-size: 1.4em; font-weight: 500;">
-                            <li><span class="fa fa-check text-success"></span> Participate in this contest</li>
-                            <li><span class="fa fa-check text-success"></span> Leaderboard feature to see who is on top</li>
-                            <li><span class="fa fa-check text-success"></span> Solve the problems faster than others to win</li>
-                            <li><span class="fa fa-check text-success"></span> Choose to code in your favorite language</li>
-                            </ul>
-                        </div>
-                        </div>
-                    </div>
-                    <!-- Registration Form -->
-                    <div class="col-sm-6 col-xs-12">
-                        <div class="row">
-                        <div class="col-xs-12">
-                            <form id="signupForm" method="post" accept-charset="UTF-8" validate>
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="fa fa-user-secret icon-2x"></i></i></span>
-                                <input id="name" class="form-control input-lg" placeholder="Full Name" maxlength="100" type="text" name="name">
-                            </div>
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="icon-user icon-2x"></i></span>
-                                <input id="handle" class="form-control input-lg" placeholder="Username" maxlength="100" type="text" name="handle">
-                            </div>
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="icon-envelope icon-2x"></i></span>
-                                <input id="email" class="form-control input-lg" placeholder="Email" maxlength="100" type="email" name="email" validate>
-                            </div>
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="icon-asterisk icon-2x"></i></span>
-                                <input id="password" class="form-control input-lg" placeholder="Password" maxlength="60" type="password" name="password">
-                            </div>
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="icon-asterisk icon-2x"></i></span>
-                                <input id="password_confirmation" class="form-control input-lg" placeholder="Confirm Password" maxlength="60" type="password" name="password_confirmation">
-                            </div>
-                            <div class="form-group">
-                                <div class="g-recaptcha" data-sitekey="6LdJRxETAAAAABZT0MUNO1r5at_-JuSGeAPTqIM6"></div>
-                            </div>
-                            <div class="form-group">
-                                <button type="submit" id="btn-signup" class="btn btn-block btn-primary btn-lg">Sign Up</button>
-                            </div>
-                            </form>
-                            <div class="form-group">
-                            <div class="topCushion">Already a member? <a href="#">Login</a></div>
-                            </div>
-                        </div>
-                        <!-- /.col-xs-12 -->
-                        </div>
-                        <!-- /.row -->
-                    </div>
-                    <!-- /.col-sm-6 col-xs-12 -->
-                    </div>
-                    <!-- /.row -->
-                </div>
-                <!-- /.well well-lg -->
-                </section>
-                <!-- /.col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-xs-12 col-sm-offset-0 -->
             </div>
-            <!-- /.row -->
+            <div class="col">
+            <div class="form-outline">
+                <input type="text" id="form3Example2" class="form-control" />
+                <label class="form-label" for="form3Example2">Last name</label>
             </div>
-            <!-- /#realContent -->
+            </div>
         </div>
-        <!-- /.row -->
+
+        <!-- Email input -->
+        <div class="form-outline mb-4">
+            <input type="email" id="form3Example3" class="form-control" />
+            <label class="form-label" for="form3Example3">Email address</label>
         </div>
-        <!-- /#contentContainer -->
+
+        <!-- Password input -->
+        <div class="form-outline mb-4">
+            <input type="password" id="form3Example4" class="form-control" />
+            <label class="form-label" for="form3Example4">Password</label>
+        </div>
+
+        <!-- Checkbox -->
+        <div class="form-check d-flex justify-content-center mb-4">
+            <input class="form-check-input me-2" type="checkbox" value="" id="form2Example33" checked />
+            <label class="form-check-label" for="form2Example33">
+            Subscribe to our newsletter
+            </label>
+        </div>
+
+        <!-- Submit button -->
+        <button type="submit" class="btn btn-primary btn-block mb-4">Sign up</button>
+
+        <!-- Register buttons -->
+        <div class="text-center">
+            <p>or sign up with:</p>
+            <button type="button" class="btn btn-secondary btn-floating mx-1">
+            <i class="fab fa-facebook-f"></i>
+            </button>
+
+            <button type="button" class="btn btn-secondary btn-floating mx-1">
+            <i class="fab fa-google"></i>
+            </button>
+
+            <button type="button" class="btn btn-secondary btn-floating mx-1">
+            <i class="fab fa-twitter"></i>
+            </button>
+
+            <button type="button" class="btn btn-secondary btn-floating mx-1">
+            <i class="fab fa-github"></i>
+            </button>
+        </div>
+    </form>
     </main>
     <svg width="100%" height="41.8%" id="intro" version="1.1" xmlns="http://www.w3.org/2000/svg">
         <path id="wave" d="" fill="#fff"/>
     </svg>
     <script src="wavy.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- MDB -->
+    <script
+    type="text/javascript"
+    src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.1/mdb.min.js"
+    ></script>
 </body>
 </html>
